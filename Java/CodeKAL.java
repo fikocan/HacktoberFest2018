a918dba4abfe6240bb62f2b18fd9bce2b28c1d68
fikocan <56916431+fikocan@users.noreply.github.com>
2019-10-23 13:27:11 +0300

    Update CodeKAL.java
    Gorev

--- Java/CodeKAL.java
+++ Java/CodeKAL.java
@@ -5,8 +5,8 @@ public class CodeKal {
 		// Aşağıdaki cümleleri ingilizceye çeviriniz.(Noktalama işaretlerinin Java için 
 		// özel anlamları var. Çevirirken silmeyiniz.)
 
-		System.out.println("CodeKAL'ı seviyorum!");
-		System.out.println("CodeKAL'ın bir parçası olduğum için çok mutluyum.");
+		System.out.println("I love CodeKAL");
+		System.out.println("I am very happy because of being part of CodeKAL");
 
 	}
 
